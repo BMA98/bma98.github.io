c98600a0b49169f85231ba0bbe74ebd0c37191cd
Bastian Matamala <bastian.matamala@ug.uchile.cl>
2022-07-21 01:12:32 -0400

    chore: change title

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,8 +9,8 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - bastian',
-    title: 'bastian',
+    titleTemplate: '%s',
+    title: 'Bastián Matamala',
     htmlAttrs: {
       lang: 'en'
     },
